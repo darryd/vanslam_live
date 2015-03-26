13992a0f8460e44d0e5783f4930791da578dda55
darryd <darry.d@gmail.com>
2015-03-25 20:34:45 -0700

    running web app in ssl only

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module VanslamLive3
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use ChatDemo::ChatBackend
+
+    config.force_ssl = true
   end
 end
