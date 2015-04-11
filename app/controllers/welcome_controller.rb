0087aecbe88cea6d20f481128821149bf4c56285
darryd <darry.d@gmail.com>
2015-04-11 16:41:45 -0700

    Logging in but without checking the password

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,75 @@
 class WelcomeController < ApplicationController
+
+  #------------------------------------------------------------------------------------#
   def index
   end
 
+  #------------------------------------------------------------------------------------#
   def login
-    flash[:session_id] = session[:session_id]
 
     flash[:is_logged_in] = is_logged_in() 
 
   end
 
+  #------------------------------------------------------------------------------------#
   def is_logged_in
     0 != LoggedIn.where(session_id: session[:session_id]).count
   end
+
+  #------------------------------------------------------------------------------------#
+
+  def check_credentials (user_name, password)
+
+    scorekeeper = Scorekeeper.where(user_name: user_name).take
+
+    if scorekeeper == nil
+      result = false
+
+    else 
+      result = true
+    end
+
+    return result
+  end
+  #------------------------------------------------------------------------------------#
+  def do_log_in
+
+    user_name = params[:user_name]
+
+    result = check_credentials(user_name, "password")
+
+    if result and not is_logged_in()
+
+      scorekeeper = Scorekeeper.where(user_name: user_name).take
+      if scorekeeper != nil # Hey maybe the scorekeeper was deleted since we checked credentials.
+
+	logged_in = LoggedIn.new
+	logged_in.scorekeeper_id = scorekeeper.id
+	logged_in.save
+	
+      else
+	result = false
+      end
+    end
+
+    render json: {:result => result}
+
+  end
+  #------------------------------------------------------------------------------------#
+
+  def do_log_out
+
+    logged_in = LoggedIn.where(session_id: session[:session_id]).take
+    
+    if logged_in != nil
+      logged_in.delete
+    end
+    
+    render json: {:message => "You are logged out."}
+  end
+
+  #------------------------------------------------------------------------------------#
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   get "/login", to: "welcome#login"
 
+  post "welcome/do_log_in"
+  get "welcome/do_log_out"
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
