37f97c7e17d2e190059208bfcf641fcca6e5258f
darryd <darry.d@gmail.com>
2015-04-11 13:41:02 -0700

    Can now check if a user is logged in.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,12 @@ class WelcomeController < ApplicationController
 
   def login
     flash[:session_id] = session[:session_id]
+
+    flash[:is_logged_in] = is_logged_in() 
+
+  end
+
+  def is_logged_in
+    0 != LoggedIn.where(session_id: session[:session_id]).count
   end
 end
