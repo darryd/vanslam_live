12cb3590e9a260f1109bbb16e5d26b976963e0a7
darryd <darry.d@gmail.com>
2015-04-12 10:41:09 -0700

    cleaning up index (I tried displaying an image, but it is just blank)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+
+  # added by darry
+  # images not displaying
+  #config.assets.compile = true
+  #config.assets.enabled = true
+
+
 end
