4d28a15f296fe3ec934a15e2bd9a459b6d5f07a3
darryd <darry.d@gmail.com>
2015-04-15 11:32:18 -0700

    Sorting sublist of poet names based on a scoring scheme.

--- app/assets/javascripts/poet_names.js
+++ app/assets/javascripts/poet_names.js
@@ -1,21 +1,52 @@
 
 /*-----------------------------------------------------------------------*/
 
+// Search for strings in 'list' containing 'str' and then sorted.
+// 
+// If list = ["Darryl", "Darry Danzig"] and str = "Darry",
+// then the new list should be sorted so that "Darry Danzig" appears
+// earlier.
+
 function subset (list, str) {
 
+  var str_lower = str.toLowerCase();
   var new_list = [];
 
   for (var i=0; i<list.length; i++) {
 
+    var match = {};
+    match.item = list[i];
+    match.score = score_match(list[i].toLowerCase(), str_lower);
 
-    var s = list[i].toLowerCase();
-    var substr = str.toLowerCase();
-
-    if (s.indexOf(substr) != -1)
-      new_list.push(list[i]);
-
+    if (match.score != Infinity)
+      new_list.push(match);
   }
+
+  new_list.sort(function(a, b) {return a.score - b.score;});
+
   return new_list;
+}
+
+/*-----------------------------------------------------------------------*/
+
+// Lower score the better.
+function score_match(str, substr) {
+
+  var score = Infinity;
+
+  if (str.indexOf(substr) != -1)
+    score = str.length;
+
+  // Now let us see if we can match substr to a whole word in str
+  // to qualify for an even better score.
+
+  var words = str.split(/\s+/);
+
+  for (var i=0; i<words.length; i++) 
+    if (substr == words[i])
+      score = substr.length;
+
+  return score;
 }
 
 /*-----------------------------------------------------------------------*/
