c089da0f4d98ac802909501c70ac0b90a2f4f031
darryd <darry.d@gmail.com>
2015-05-22 10:18:19 -0700

    filting logs

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,6 @@ module VanslamLive3
     config.middleware.use ChatDemo::ChatBackend
 
     config.force_ssl = true
-    config.filter_parameters += ['passwd', 'session_id', 'authenticity_token']
+    config.filter_parameters += ['passwd', 'authenticity_token']
   end
 end
