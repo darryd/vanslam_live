4eae073de9157eed13b3b9ec760715d983521087
darryd <darry.d@gmail.com>
2015-07-24 20:01:03 -0700

    added function to build titles of performance div

--- app/assets/javascripts/performance_div.js
+++ app/assets/javascripts/performance_div.js
@@ -4,13 +4,14 @@
 function performance_div_new(performance) {
 
   var div = document.createElement("div");
-  div.structure = {"Judges": slam.num_judges, "Time": 2, "Penalty": 1, "Score": 1, "Subscore": 1, "Total Score": 1, "Rank":1};
-  div.order = ["Judges", "Time", "Penalty", "Score", "Subscore", "Total Score", "Rank"];
+  div.column_length = {"Judges": slam.num_judges, "Time": 2, "Penalty": 1, "Score": 1, "Subscore": 1, "Total Score": 1, "Rank":1};
+  div.titles = ["Judges", "Time", "Penalty", "Score", "Subscore", "Total Score", "Rank"];
   performance_div_build_columns_hash(div);
 
   div.performance = performance;
   div.id = makeid(20);
-  div.className = "row";
+
+  perf_div_build_titles_row(div);
 
   return div;
 }
@@ -21,19 +22,37 @@ function performance_div_build_columns_hash(div) {
   var num_columns = 12;
 
   var columns = div.columns = {};
-  var titles = div.order;
+  var titles = div.titles;
 
   var remaining_columns = num_columns;
   for (var i=0; i<titles.length; i++) {
     columns[titles[i]] = remaining_columns;
-    remaining_columns -= div.structure[titles[i]];
+    remaining_columns -= div.column_length[titles[i]];
 
     if (i > 0) {
       for (j=0; j<i; j++) {
-	columns[titles[j]] = div.structure[titles[j]];
+	columns[titles[j]] = div.column_length[titles[j]];
       }
     }
   }
+}
+/*----------------------------------------------------------------------------------------------------------------------------------*/
+function perf_div_build_titles_row(div) {
+
+  var column;
+  var row = document.createElement("div");
+  row.className = "row";
+
+  for (var i=0; i<div.titles.length; i++) {
+    
+    column = document.createElement("div");
+    column.className = "large-" + div.columns[div.titles[i]] + " columns";
+    column.innerHTML = div.titles[i];
+
+    row.appendChild(column);
+
+  }
+  div.appendChild(row);
 }
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 
