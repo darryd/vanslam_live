efb78ca0fb293496828fb67524d81e33438b6200
darryd <darry.d@gmail.com>
2015-07-24 19:41:30 -0700

    adding function performance_div_build_columns_hash()

--- app/assets/javascripts/performance_div.js
+++ app/assets/javascripts/performance_div.js
@@ -6,6 +6,7 @@ function performance_div_new(performance) {
   var div = document.createElement("div");
   div.structure = {"Judges": slam.num_judges, "Time": 2, "Penalty": 1, "Score": 1, "Subscore": 1, "Total Score": 1, "Rank":1};
   div.order = ["Judges", "Time", "Penalty", "Score", "Subscore", "Total Score", "Rank"];
+  performance_div_build_columns_hash(div);
 
   div.performance = performance;
   div.id = makeid(20);
@@ -15,17 +16,24 @@ function performance_div_new(performance) {
 }
 
 /*----------------------------------------------------------------------------------------------------------------------------------*/
-function performance_div_build_headings(performance_div) {
+function performance_div_build_columns_hash(div) {
 
+  var num_columns = 12;
 
-  var div = document.createElement("div");
+  var columns = div.columns = {};
+  var titles = div.order;
 
+  var remaining_columns = num_columns;
+  for (var i=0; i<titles.length; i++) {
+    columns[titles[i]] = remaining_columns;
+    remaining_columns -= div.structure[titles[i]];
 
-
-
+    if (i > 0) {
+      for (j=0; j<i; j++) {
+	columns[titles[j]] = div.structure[titles[j]];
+      }
+    }
+  }
 }
-
-
-
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 
