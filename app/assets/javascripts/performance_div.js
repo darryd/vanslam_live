fa2515518e173515d5dd4fde2263f9e346c50569
darryd <darry.d@gmail.com>
2015-08-24 16:35:10 -0700

    setting input.type = "number" to make it easier to enter scores/times/penalties

--- app/assets/javascripts/performance_div.js
+++ app/assets/javascripts/performance_div.js
@@ -291,6 +291,7 @@ function p_div_build_data_row(div) {
     input.setAttribute('size', 4);
     input.setAttribute('onchange', 'p_div_input_entered(this)');
     input.setAttribute('data-index', i);
+    input.type = "number";
 
     if (!login_info.is_logged_in)
       input.setAttribute('readonly', null);
