7bf2445bb4a3db513f56b4b0b4884dcc30b529c0
darryd <darry.d@gmail.com>
2015-08-24 21:16:27 -0700

    bigger input

--- app/assets/javascripts/performance_div.js
+++ app/assets/javascripts/performance_div.js
@@ -292,7 +292,7 @@ function p_div_build_data_row(div) {
     input.setAttribute('onchange', 'p_div_input_entered(this)');
     input.setAttribute('data-index', i);
     input.type = "number";
-    input.style.width = "50px";
+    input.style.width = "60px";
 
     if (!login_info.is_logged_in)
       input.setAttribute('readonly', null);
