e67d71ab38a7439c664c34a6b563ce039c2b8d15
darryd <darry.d@gmail.com>
2016-11-21 15:06:23 -0800

    working on form for editing a round

--- app/assets/javascripts/edit_round.js
+++ app/assets/javascripts/edit_round.js
@@ -18,12 +18,18 @@ function onclick_edit_round() {
 
   edit_round_div.appendChild(hide_a);
 
-  var info_div = make_info_div(round);
+  //var info_div = make_info_div(round);
+
+  var form_div = make_submit_round_info(round); // TODO Rename this
   
-  edit_round_div.appendChild(info_div);
+  //edit_round_div.appendChild(info_div);
+  edit_round_div.appendChild(form_div);
+
+
   $(hide_a).click(function() {
     this.remove();
-    info_div.remove();
+    //info_div.remove();
+    form_div.remove();
     $(edit_round_a).show();
   });
 
@@ -31,14 +37,12 @@ function onclick_edit_round() {
 
 }
 
-function make_info_div (round) {
+function make_info_div(round) {
 
   var info_div = document.createElement("p");
 
-  info_str = "<strong>Title: </strong>" + round.title;
-  info_str = info_str + ",<strong> Number Of Poets: </strong> " + round.num_poets;
+  info_str = "<strong> Number Of Poets: </strong> " + round.num_poets;
   info_str = info_str + ",<strong> Number of Places:  </strong>" + round.num_places;
-  info_str = info_str + ",<strong> Title:  </strong>" + round.title;
   info_str = info_str + ",<strong> Time limit:  </strong>" + round.time_limit;
   info_str = info_str + ",<strong> Grace period:  </strong>" + round.grace_period;
   info_str = info_str + ",<strong> Is Cumulative:  </strong>" + round.is_cumulative;
@@ -50,4 +54,39 @@ function make_info_div (round) {
 
 
   return info_div;
+}
+
+// TODO Rename this.
+function make_submit_round_info(round) {
+  
+
+  var row;
+
+  row = document.createElement('div');
+  row.className = "row";
+
+  add_label_input_to_row(row, "Title", round.title);
+  add_label_input_to_row(row, "Number of poets", round.num_poets);
+  add_label_input_to_row(row, "Number of places", round.num_places);
+  add_label_input_to_row(row, "Time limit", round.time_limit);
+  add_label_input_to_row(row, "Grace period", round.grace_period);
+  add_label_input_to_row(row, "Is cumulative", round.is_cumulative);
+  add_label_input_to_row(row, "Previous round number", round.previous_round_number);
+
+  return row;
+}
+
+function add_label_input_to_row (row, label_text, input_value) {
+
+  var column = document.createElement('div');
+  column.className = "small-2 columns";
+  row.appendChild(column);
+
+  var label = document.createElement('label');
+  label.innerHTML = label_text;
+  column.appendChild(label);
+
+  var input = document.createElement('input');
+  label.appendChild(input);
+  input.value = input_value;
 }
