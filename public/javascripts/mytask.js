7c2e82d6a3174cbb0b04058f49d291be99c4c584
Darry Danzig <darry.d@gmail.com>
2017-03-18 18:21:16 -0700

    working on using a Web Worker for onkeyup and onkeydown messsages

--- public/javascripts/mytask.js
+++ public/javascripts/mytask.js
@@ -1,2 +1,30 @@
+
 self.addEventListener('message', function(e) {
+
+    function connect(url) {
+
+        self.web_sock = new WebSocket(url);
+
+    }
+
+    function send(data) {
+
+        self.web_sock.send(data);
+
+    }
+
+    console.log("I am a worker");
+
+    var data = JSON.parse(e.data)
+
+    switch (data['type']) {
+
+        case 'url': 
+            connect(data['url']);
+            break;
+
+        case 'send':
+            send(data['data']);
+            break;
+    };
 }, false);
