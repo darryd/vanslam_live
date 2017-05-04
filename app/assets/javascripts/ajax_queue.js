1f3f02d82082f893f9b9d50217a6bc7b13c6fe4f
Darry Danzig <darry.d@gmail.com>
2017-05-04 15:51:12 -0700

    indenting

--- app/assets/javascripts/ajax_queue.js
+++ app/assets/javascripts/ajax_queue.js
@@ -10,143 +10,143 @@ var DEFAULT_COLOR = "blue";
 function process_ajax_queue() {
 
 
-  display_queue();
-  
+	display_queue();
 
-   if (window.ajax_queue.length == 0)
-     return;
-   
-   if (window.ajax_queue[0].state == PENDING) {
-     window.ajax_queue[0].clicks++;
 
-     // Experimental
-     if (window.ajax_queue[0].clicks == 4000)
-       window.ajax_queue[0].state = START;
+	if (window.ajax_queue.length == 0)
+		return;
 
-     return;
-   }
+	if (window.ajax_queue[0].state == PENDING) {
+		window.ajax_queue[0].clicks++;
 
-   if (window.ajax_queue[0].state == DONE) {
-     window.ajax_queue.splice(0,1);
-     return;
-   }
+		// Experimental
+		if (window.ajax_queue[0].clicks == 4000)
+			window.ajax_queue[0].state = START;
 
-   if (window.ajax_queue[0].state == START)
-     start_next(window.ajax_queue[0]);
+		return;
+	}
+
+	if (window.ajax_queue[0].state == DONE) {
+		window.ajax_queue.splice(0,1);
+		return;
+	}
+
+	if (window.ajax_queue[0].state == START)
+		start_next(window.ajax_queue[0]);
 
 }
 /*-----------------------------------------------------------------------*/
 function display_queue() {
 
-  var p = document.getElementById("ajax_queue");
+	var p = document.getElementById("ajax_queue");
 
-  if (p != null)
-    p.innerHTML = ajax_queue_to_str();
+	if (p != null)
+		p.innerHTML = ajax_queue_to_str();
 
 }
 /*-----------------------------------------------------------------------*/
 function ajax_queue_to_str() {
 
-  var str = "Queue: ";
+	var str = "Queue: ";
 
-  for (var i = 0; i < window.ajax_queue.length; i++) {
-    switch (window.ajax_queue[i].state) {
-      case START:
-	str += " S ";
-	break;
-      case PENDING:
-	str += " P ";
-	break;
-      case DONE:
-	str += " D ";
-	break;
-    }
-  }
+	for (var i = 0; i < window.ajax_queue.length; i++) {
+		switch (window.ajax_queue[i].state) {
+			case START:
+				str += " S ";
+				break;
+			case PENDING:
+				str += " P ";
+				break;
+			case DONE:
+				str += " D ";
+				break;
+		}
+	}
 
-  return str;
+	return str;
 }
 /*-----------------------------------------------------------------------*/
 function display_messages_if_any(response_json) {
 
-  var clear_message_interval = 5000;
+	var clear_message_interval = 5000;
 
-  if (response_json.message != undefined) {
+	if (response_json.message != undefined) {
 
 
-    var messages = document.getElementById('messages');
-    if (messages != null) {
+		var messages = document.getElementById('messages');
+		if (messages != null) {
 
-      if (messages.timer_id != undefined)
-	clearTimeout(messages.timer_id);
+			if (messages.timer_id != undefined)
+				clearTimeout(messages.timer_id);
 
-      messages.innerHTML = "Server: " + response_json.message;
-      messages.style.color = response_json.result ? DEFAULT_COLOR : ERROR_COLOR;
+			messages.innerHTML = "Server: " + response_json.message;
+			messages.style.color = response_json.result ? DEFAULT_COLOR : ERROR_COLOR;
 
-      messages.timer_id = setTimeout(function() {
+			messages.timer_id = setTimeout(function() {
 
-	var messages = document.getElementById('messages');
-	if (messages.style.color != ERROR_COLOR) {
-	  messages.innerHTML = "";
+				var messages = document.getElementById('messages');
+				if (messages.style.color != ERROR_COLOR) {
+					messages.innerHTML = "";
+				}
+
+			}, clear_message_interval);
+		}
 	}
-
-      }, clear_message_interval);
-    }
-  }
 }
 /*-----------------------------------------------------------------------*/
 function start_next(ticket) {
 
-  ticket.state = PENDING;
-  ticket.clicks = 0;
+	ticket.state = PENDING;
+	ticket.clicks = 0;
 
-  ticket.xmlhttp = get_xmlhttp();
+	ticket.xmlhttp = get_xmlhttp();
 
-  // Run ticket.done when request is complete.
-  ticket.xmlhttp.onreadystatechange = function() {
+	// Run ticket.done when request is complete.
+	ticket.xmlhttp.onreadystatechange = function() {
 
-    console.log(ticket.xmlhttp);
-    console.log(['ReadyState', ticket.xmlhttp.readyState]);
-    console.log(['status', ticket.xmlhttp.status]);
+		console.log(ticket.xmlhttp);
+		console.log(['ReadyState', ticket.xmlhttp.readyState]);
+		console.log(['status', ticket.xmlhttp.status]);
 
-    try {
-      if (ticket.xmlhttp.readyState==4) {
-	if (ticket.xmlhttp.status==200) {
-	  // Run done function.
+		try {
+			if (ticket.xmlhttp.readyState==4) {
+				if (ticket.xmlhttp.status==200) {
+					// Run done function.
 
-	  var response_json = jQuery.parseJSON(ticket.xmlhttp.responseText);
-	  display_messages_if_any(response_json);
-	  ticket.done(response_json);
-	  ticket.state = DONE;
-	}
-	else {
+					var response_json = jQuery.parseJSON(ticket.xmlhttp.responseText);
+					display_messages_if_any(response_json);
+					ticket.done(response_json);
+					ticket.state = DONE;
+				}
+				else {
 
-	  // Try again
-	  ticket.state = START;
-	}
-      }
-    }
-    catch(e) {
+					// Try again
+					ticket.state = START;
+				}
+			}
+		}
+		catch(e) {
 
-      console.log(e);
-      alert ("Error " + e.description);
-      console.log(e.description);
-    }
-  };
+			console.log(e);
+			alert ("Error " + e.description);
+			console.log(e.description);
+		}
+	};
 
-  // Timeout
-  ticket.xmlhttp.timeout = 9000;
-  ticket.xmlhttp.ontimeout = function() {
-    ticket.state = START;
-    console.log("Request timed out");
-  };
+	// Timeout
+	ticket.xmlhttp.timeout = 9000;
+	ticket.xmlhttp.ontimeout = function() {
+		ticket.state = START;
+		console.log("Request timed out");
+	};
 
-  // If there's an error reset to START
-  ticket.xmlhttp.onerror = function() {
-    ticket.state = START;
-  };
+	// If there's an error reset to START
+	ticket.xmlhttp.onerror = function() {
+		ticket.state = START;
+	};
 
-  // Send request
-  post_async(ticket.xmlhttp, ticket.url, ticket.get_params());
+	// Send request
+	post_async(ticket.xmlhttp, ticket.url, ticket.get_params());
 
 }
 /*-----------------------------------------------------------------------*/
@@ -154,21 +154,21 @@ function start_next(ticket) {
 // See send_echo_request() to see how this is done.
 
 function new_ticket() {
-  return {state: START, url: '', get_params: null, done: null};
+	return {state: START, url: '', get_params: null, done: null};
 }
 
 /*-----------------------------------------------------------------------*/
 
 function send_echo_request(sentence) {
 
-  var ticket = new_ticket();
+	var ticket = new_ticket();
 
-  ticket.url = "/competition/echo";
+	ticket.url = "/competition/echo";
 
-  ticket.get_params = function () {return {"sentence": sentence};};
+	ticket.get_params = function () {return {"sentence": sentence};};
 
-  ticket.done = function(response_json) {console.log(response_json);};
+	ticket.done = function(response_json) {console.log(response_json);};
 
-  window.ajax_queue.push(ticket);
+	window.ajax_queue.push(ticket);
 }
 /*-----------------------------------------------------------------------*/
