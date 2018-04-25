c84bf44a9c713ec5a89121fdd6d47bdbb48f9c04
= <darry.d@gmail.com>
2018-04-24 19:57:47 -0700

    season 3

--- app/middleware/chat_backend.rb
+++ app/middleware/chat_backend.rb
@@ -277,7 +277,7 @@ module MakeSlam
 
 		c = Competition.create(event_number: 0, num_judges: 5, do_not_include_min_and_max_scores:true, organization_id: 2)
 		c.title = title
-		c.season_id = 2
+		c.season_id = 3
 		c.save
 
 		Round.create(competition_id: c.id,
